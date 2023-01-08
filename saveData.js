df6e4d2ab638e8057c0fd83baf7c8999754b8f68
Marshall Brown <marshallendel@gmail.com>
2023-01-08 06:21:14 -0500

    add download stats functionality

--- saveData.js
+++ saveData.js
@@ -1,17 +1,18 @@
 import fs from "fs/promises";
-import fetch from 'node-fetch'
+import fetch from "node-fetch";
 import path from "path";
 
 const saveData = async (data, options) => {
   if (options.length) {
     if (options.includes("Stats")) {
-      console.log("download stats");
+      const stats = getStats(data);
+      downloadStats(data.name, stats);
     }
     if (options.includes("Sprites")) {
-        const sprites = getSprites(data)
-        sprites.forEach(([spriteName, url]) => {
-            downloadPicture(data.name, spriteName, url)
-        })
+      const sprites = getSprites(data);
+      sprites.forEach(([spriteName, url]) => {
+        downloadPicture(data.name, spriteName, url);
+      });
     }
     if (options.includes("Artwork")) {
       console.log("download artwork!");
@@ -21,21 +22,49 @@ const saveData = async (data, options) => {
   }
 };
 
+const getStats = (data) => {
+  const stats = {};
+
+  Object.entries(data.stats).forEach(([, { base_stat, stat }]) => {
+    stats[stat.name] = base_stat;
+  });
+
+  return `HP: ${stats.hp}
+Attack: ${stats.attack}
+Defense: ${stats.defense}
+Special Attack: ${stats["special-attack"]}
+Special Defense: ${stats["special-defense"]}
+Speed: ${stats.speed}`;
+};
+
 const getSprites = (data) => {
-    return Object.entries(data.sprites).filter(([spriteName, url]) =>  (spriteName.includes('front') || spriteName.includes('back')) && url)
-}
+  return Object.entries(data.sprites).filter(
+    ([spriteName, url]) =>
+      (spriteName.includes("front") || spriteName.includes("back")) && url
+  );
+};
 
 const downloadPicture = async (name, fileName, url) => {
-    try {
-      const res = await fetch(url);
-      const imageData = await res.arrayBuffer();
-      const buffer = Buffer.from(imageData);
-      await fs.mkdir(name, {recursive: true})
-      await fs.writeFile(`./${name}/${fileName}.png`, buffer);
-      console.log(`${fileName}.png downloaded!`)
-    } catch (err) {
-      console.error(err);
-    }
-  };
-  
+  try {
+    const res = await fetch(url);
+    const imageData = await res.arrayBuffer();
+    const buffer = Buffer.from(imageData);
+    await fs.mkdir(name, { recursive: true });
+    await fs.writeFile(`./${name}/${fileName}.png`, buffer);
+    console.log(`${fileName}.png downloaded!`);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const downloadStats = async (name, stats) => {
+  try {
+    await fs.mkdir(name, { recursive: true });
+    await fs.writeFile(`./${name}/stats.text`, stats);
+    console.log(`stats.text downloaded!`);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 export default saveData;
